f02559f41b41e8e9d39c5e00c7d282ac56dba2a4
okianya <mitenkoann@gmail.com>
2026-04-05 23:21:14 +0300

    Настройка Vitest

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import strong from './assets/strong.jpg'
+import './App.css'
+
+function App() {
+	return (
+				<div>
+					<h1>✦ проект на React ✦</h1>
+					<img src={strong} width="250"/>
+				</div>
+	)
+}
+
+export default App
